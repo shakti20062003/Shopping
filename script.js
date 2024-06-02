38d3b5f680d9255787156f6edf850373f172f72c
shakti20062003 <133684769+shakti20062003@users.noreply.github.com>
2024-06-02 19:58:05 +0530

    added all remaining files

---
+++ script.js
@@ -0,0 +1,16 @@
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
+const close = document.getElementById('close');
+
+if(bar){
+    bar.addEventListener('click',()=>{
+        nav.classList.add('active');
+        console.log("clicked");
+    })
+}
+if(close){
+    close.addEventListener('click',()=>{
+        nav.classList.remove('active');
+        console.log("Closed clicked");
+    })
+}
